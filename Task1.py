86c6420ed92dd773329ee4047370ec8da262ab3c
solankiyashraj155-svg <solankiyashraj155@gmail.com>
2025-11-16 23:17:24 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+num1 = float(input('Enter first number'))
+num2 = float(input('Enter second number'))
+Addition = float(num1) + float(num2)
+Subtraction = float(num1) - float(num2)
+Multiplication = float(num1) * float(num2)
+Division = float(num1) / float(num2)
+print('Addition :',Addition)
+print('Subtraction :',Subtraction)
+print('Multiplication :',Multiplication)
+print('Division :',Division)
+
+
+
---
+++ Task2.py
@@ -0,0 +1,4 @@
+name1 = input('Enter first name')
+name2 = input('Enter second name')
+fullname = name1 + ' ' + name2
+print('Hello Welcome',fullname,'!','Welcome to the Python Program')
